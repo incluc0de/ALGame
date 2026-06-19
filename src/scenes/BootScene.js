621f176b42bee589be47370001fe2791123314a9
incluc0de <incluc0de.ai@gmail.com>
2026-06-19 18:10:34 -0300

    correçao no bootScene

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
 import { GameState } from "../state/GameState.js";
 export default class BootScene extends Phaser.Scene {
   constructor() { super("BootScene"); }
-  create() { GameState.load(); this.scene.start("MenuScene"); }
+  create() { GameState.load(); this.scene.start("PreGameXPScene"); }
 }
